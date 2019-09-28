9695f77e0f44e54084f4b3bc41c3fde1a2dbf31a
whygreedy <cindyyeah9@gmail.com>
2019-09-29 00:11:57 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -7,10 +7,12 @@ with open('reviews.txt', 'r') as r:
 		count = count + 1
 		if count % 1000 == 0: # %求餘數
 			print(len(data))
+print('檔案讀取完了，總共用', len(data), '筆資料')
 
 
+sum_len = 0
+for d in data:
+	sum_len += len(d)
 
-print(len(data))
-print(data[0])
-print('-----分隔線-----')
-print(data[1])
+print('每一筆留言的平均長度為', sum_len/len(data))
+
