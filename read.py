b96405a3eca422b519f4baa0d735e29e5d1dec3a
whygreedy <cindyyeah9@gmail.com>
2019-09-29 14:24:08 +0800

    add filtering

--- read.py
+++ read.py
@@ -16,3 +16,12 @@ for d in data:
 
 print('每一筆留言的平均長度為', sum_len/len(data))
 
+
+# 篩選出留言長度 < 100字的留言數
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
